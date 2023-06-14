044cb7c5840a9f9c6282f6891e5636522ef1bf0c
Bright11 <chikanwazuo@gmail.com>
2023-06-14 05:32:16 -0700

    categories

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name="newsapp"
 urlpatterns = [
-    path('', views.index.as_view(),name='index')
+    path('', views.index.as_view(),name='index'),
+    path("category/<int:pk>/",views.category.as_view(),name="category")
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -8,8 +8,22 @@ class index(View):
         catenews=Category.objects.filter(category__isnull=False).distinct()[:3]
         newsslide=News.objects.all()
         latest=News.objects.order_by("created_at")[:2]
-        constext={"title":"bright python news website","catenews":catenews,"latest":latest,"newsslide":newsslide}
+        cat=Category.objects.all()
+        constext={"title":"bright python news website","cat":cat, "catenews":catenews,"latest":latest,"newsslide":newsslide}
         return render(request,"pages/index.html",constext)
 
     def post(self,request):
+        pass
+
+
+class category(View):
+    def get(self, request,pk):
+        category=News.objects.filter(category=pk)
+        latest=News.objects.order_by("created_at")[:2]
+        newsslide=News.objects.all()
+        cat=Category.objects.all()
+        context={"category":category,"latest":latest,"cat":cat,"newsslide":newsslide}
+        return render(request,"pages/category.html",context)
+
+    def post(self, request):
         pass
